85cb72c9ad9e2e35f60cbafb0480b9079355f3ab
Parcelli <85280870+Parcelli@users.noreply.github.com>
2022-05-10 19:57:26 +0300

    Create Q1-script.sh
    Alignment script

---
+++ DZD-quiz/Q1-script.sh
@@ -0,0 +1,18 @@
+#Alignment script
+#Created by Parcelli Jepchirchir on  6th May 2022
+#Aligning reads to  a refence genome(mecA gene) using BWA
+#Step 1 : indexing the reference genome
+```
+#bwa index -p mecA ../mecA.fa
+```
+#Step 2: Aligning reads to the mecA gene using bwa mem
+```
+for file in *.1.fastq;
+do
+    name=$(basename ${file} .1.fastq)
+    echo ${name}
+    bwa mem  ../mecA  ${file} ${name}.2.fastq > ${name}.sam
+    samtools view -h  -b  ${name}.sam >${name}.bam
+done
+# Step 3: convert sam files to bam files
+#samtools view -h  -b  ${name}.sam >${name}.bam
