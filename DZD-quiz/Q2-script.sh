f7e39eaee5288fb853aa479ae59d8d819c36675b
Parcelli <85280870+Parcelli@users.noreply.github.com>
2022-05-14 00:19:24 +0300

    Update and rename Q2-script.sh to coverage.py

--- DZD-quiz/Q2-script.sh
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env bash
-#Calculating genome coverage using bedtools genomecov
-#input = sorted bam files
-#output = bedgraph
-for file in *sorted.bam
-do
-    echo ${file}
-    bedtools genomecov -max $1 -ibam ${file} -bga > ${file}.txt
-done
