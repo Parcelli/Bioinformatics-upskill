c062cd8979ec81087fad03cacd423be17ad08a48
Parcelli <85280870+Parcelli@users.noreply.github.com>
2022-05-13 14:20:33 +0300

    Update coverage.sh

--- DZD-quiz/Scripts/coverage.sh
+++ DZD-quiz/Scripts/coverage.sh
@@ -1,6 +1,6 @@
 #!usr/bin/env bash
 #Computing genome coverage using bedtools genomecov.The output is a bedgraph consisting of chrom,start ,end and the depth columns.
-for file in *sorted.bam
+for file in *.bam
 do
     echo ${file}
     bedtools genomecov  -ibam ${file} -bg  > ${file}.txt
