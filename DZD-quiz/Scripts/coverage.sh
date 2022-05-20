0a93e1abaf52e0cc7d5ba4c794810ab17ef3ff5f
Parcelli <85280870+Parcelli@users.noreply.github.com>
2022-05-20 12:16:43 +0300

    Delete coverage.sh

--- DZD-quiz/Scripts/coverage.sh
+++
@@ -1,7 +0,0 @@
-#!usr/bin/env bash
-#Computing genome coverage using bedtools genomecov.The output is a bedgraph consisting of chrom,start ,end and the depth columns.
-for file in *.bam
-do
-    echo ${file}
-    bedtools genomecov  -ibam ${file} -bg  > ${file}.txt
-done
